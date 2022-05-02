e60fd87fc27929179ba881c15bead1022ae67d1c
Mammedov <39202480+Begmyrat@users.noreply.github.com>
2022-05-02 19:27:32 +0300

    retrofit is initialized

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.ui.utils
+
+class Constants {
+    companion object{
+        const val API_KEY = "dc205e5ba5ca419889e5db6fd49b34db"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
