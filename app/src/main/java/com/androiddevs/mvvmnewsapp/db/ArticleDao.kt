67d2b0e4d7b63eb100cf97211a8cb85ff862dc14
Mammedov <39202480+Begmyrat@users.noreply.github.com>
2022-05-03 16:11:14 +0300

    breaking news and search are implemented

--- app/src/main/java/com/androiddevs/mvvmnewsapp/db/ArticleDao.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/db/ArticleDao.kt
@@ -1,8 +1,8 @@
-package com.androiddevs.mvvmnewsapp.ui.db
+package com.androiddevs.mvvmnewsapp.db
 
 import androidx.lifecycle.LiveData
 import androidx.room.*
-import com.androiddevs.mvvmnewsapp.ui.models.Article
+import com.androiddevs.mvvmnewsapp.models.Article
 
 @Dao
 interface ArticleDao {
--- app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
@@ -1,8 +1,9 @@
-package com.androiddevs.mvvmnewsapp.ui.utils
+package com.androiddevs.mvvmnewsapp.utils
 
 class Constants {
     companion object{
         const val API_KEY = "dc205e5ba5ca419889e5db6fd49b34db"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
